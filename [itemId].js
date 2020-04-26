66bdeb7554744f7038cdc590cae8cccecb731453
patrickheger4867 <60371829+patrickheger4867@users.noreply.github.com>
2020-04-26 12:49:17 -0500

    Add files via upload

---
+++ [itemId].js
@@ -0,0 +1,42 @@
+import fetch from 'isomorphic-unfetch';
+import uuid from 'uuid';
+
+const addItemToCart = async (itemId) => {
+    const customerResponse = await fetch(`http://localhost:5555/customers`);
+    const [customer] = await customerResponse.json();
+    const cartResponse = await fetch(`http://localhost:5555/customers/${customer.customerId}/carts`);
+    const [cart] = await cartResponse.json();
+    await fetch(`http://localhost:5555/cart-items`, {
+        method: 'POST',
+        body: JSON.stringify({
+            cartItemId: uuid.v4(),
+            cartId: cart.cartId,
+            itemId,
+            quantity: 1
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+};
+
+const Index = props => (
+    <Layout>
+        <h1>Item Information</h1>
+        <img src={props.item.image}/>
+        <p>Description: {props.item.description}</p>
+        <p>Price: {props.item.price}</p>
+        <button type={"button"} onClick ={() => addItemToCart(props.item.itemId)}>
+            add to the cart
+        </button>
+    </Layout>
+);
+
+Index.getInitialProps = async function(context) {
+    const { itemId } = context.query;
+    const res = await fetch(`https://localhost:8000/items/${itemId}`);
+    const item = await res.json();
+    return {item};
+};
+
+export default Index;
